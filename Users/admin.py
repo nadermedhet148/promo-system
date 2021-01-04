8805b15c4fe2168cd4b86c8f4ea4141d34f526b5
nader <nader@192.168.1.12>
2021-01-04 23:03:24 +0200

    [feat] add NormalUser and AdministratorUser models

--- Users/admin.py
+++ Users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import AdministratorUser, NormalUser
 
-# Register your models here.
+class AdministratorUserAdmin(admin.ModelAdmin):
+    list_display = ("username", "name", "address")
+
+class NormalUserAdmin(admin.ModelAdmin):
+    list_display = ("username", "name", "address", "mobileNumber")
+
+
+admin.site.register(AdministratorUser, AdministratorUserAdmin)
+admin.site.register(NormalUser, NormalUserAdmin)
+
--- Users/models.py
+++ Users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+
+class AdministratorUser(models.Model):
+    name = models.CharField(("Name"), max_length=255)
+    username = models.CharField(("UserName"), max_length=255)
+    address = models.CharField(("Address"), max_length=255)
+    class Meta:
+        verbose_name = "Administrator_User"
+        verbose_name_plural = "Administrator_Users"
+
+class NormalUser(models.Model):
+    name = models.CharField(("Name"), max_length=255)
+    username = models.CharField(("UserName"), max_length=255)
+    address = models.CharField(("Address"), max_length=255)
+    mobileNumber = models.CharField(("Mobile_Number"), max_length=255)
+
+    class Meta:
+        verbose_name = "Normal_User"
+        verbose_name_plural = "Normal_Users"
